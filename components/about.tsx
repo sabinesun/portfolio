969bc6fac42b2d62d2e260d8fe0111db65d98736
Sabine Sun <sabine.sun@hotmail.com>
2023-09-23 16:43:52 +0200

    feat: add Memoji video

--- components/about.tsx
+++ components/about.tsx
@@ -7,15 +7,28 @@ export const About = () => {
     <div className="flex h-screen" id="about">
       <div className="m-6 flex flex-col flex-wrap justify-center">
         <div className="py-4 text-5xl font-bold text-black">À propos</div>
-        <div>
-          Actuellement en 5ème année à l'ESILV dans la majeure Cybersécurité &
-          Cloud Computing. Je vise à devenir une développeuse fullstack avec une
-          préférence pour le frontend à la sortie de l'école.
-        </div>
-        <div>
-          <Button className="mb-2" variant="outline">
-            <UploadIcon className="mr-2 h-4 w-4" /> Télécharger le CV
-          </Button>
+
+        <div className="sm:flex sm:flex-row-reverse  ">
+          <video
+            autoPlay
+            className="w-full overflow-hidden sm:w-96 md:w-4/5"
+            loop
+            muted
+            playsInline
+            src="MemojiVideo.mov"
+          />
+          <div className="flex flex-col justify-center">
+            <div>
+              Actuellement en 5ème année à l'ESILV dans la majeure Cybersécurité
+              & Cloud Computing. Je vise à devenir une développeuse fullstack
+              avec une préférence pour le frontend à la sortie de l'école.
+            </div>
+            <div className="mt-6 flex w-full justify-center">
+              <Button className="mb-2" variant="outline">
+                <UploadIcon className="mr-2 h-4 w-4" /> Télécharger le CV
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
