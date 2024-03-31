6671209dc0e254152a6d739cd81172ad7c5b3176
Sabine Sun <sabine.sun@hotmail.com>
2024-03-31 21:12:27 +0200

    chore: add prettier + eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+// eslint-disable-next-line canonical/filename-match-exported
+import { type Config } from "tailwindcss";
 
 const config: Config = {
   content: [
@@ -6,15 +7,15 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
 };
 export default config;
