619e90953ce8f5fe2b96f784d1ee0eda79f997fd
Sabine Sun <sabine.sun@hotmail.com>
2024-04-01 16:15:27 +0200

    feat: add link

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,17 +1,32 @@
 // eslint-disable-next-line canonical/filename-match-exported
 import { ModeToggle } from "@/components/theme/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="m-10 flex justify-center">
-      <div className="flex w-full justify-between md:max-w-2xl">
+      <div className="flex w-full items-center justify-between md:max-w-2xl">
         <div className="flex flex-col">
           <h1 className="text-lg font-bold">Sabine Sun</h1>
           <h2 className="text-xs font-light leading-3 text-muted-foreground">
             Développeuse front-end
           </h2>
         </div>
-        <ModeToggle />
+        <div className="flex gap-0.5">
+          <Button asChild className="w-9 p-0" variant="ghost">
+            <Link href="https://www.linkedin.com/in/sabinesun/">
+              <LinkedInLogoIcon className="h-[1.2rem] w-[1.2rem]" />
+            </Link>
+          </Button>
+          <Button asChild className="w-9 p-0" variant="ghost">
+            <Link href="https://github.com/sabinesun">
+              <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
+            </Link>
+          </Button>
+          <ModeToggle />
+        </div>
       </div>
     </nav>
   );
