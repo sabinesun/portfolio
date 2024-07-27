919633a15b0acaae5aefd8fadc14f8a28073bec8
Sabine Sun <sabine.sun@hotmail.com>
2024-07-27 18:01:27 +0200

    feat: add mail in the navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,29 +1,42 @@
+import resume from "../../resume.json";
 import { ModeToggle } from "@/components/theme/mode-toggle";
 import { Button } from "@/components/ui/button";
-import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import {
+  EnvelopeClosedIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+} from "@radix-ui/react-icons";
 import Link from "next/link";
 
 const Navbar = () => {
+  const { basics } = resume;
+
   return (
     <nav className="m-10 flex justify-center">
       <div className="flex w-full items-center justify-between md:max-w-xl">
         <div className="flex flex-col">
-          <h1 className="text-lg font-bold">Sabine Sun</h1>
+          <h1 className="text-lg font-bold">{basics.name}</h1>
           <p className="text-xs font-light leading-3 text-muted-foreground">
-            Développeuse Front-End
+            {basics.label}
           </p>
         </div>
         <div className="flex gap-0.5">
           <Button asChild className="w-9 p-0" variant="ghost">
-            <Link href="https://www.linkedin.com/in/sabinesun/">
+            <Link href={`mailto: ${basics.email}`}>
+              <EnvelopeClosedIcon className="h-[1.2rem] w-[1.2rem]" />
+            </Link>
+          </Button>
+          <Button asChild className="w-9 p-0" variant="ghost">
+            <Link href={basics.socialMedia.linkedIn.url}>
               <LinkedInLogoIcon className="h-[1.2rem] w-[1.2rem]" />
             </Link>
           </Button>
           <Button asChild className="w-9 p-0" variant="ghost">
-            <Link href="https://github.com/sabinesun">
+            <a href={basics.socialMedia.github.url}>
               <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
-            </Link>
+            </a>
           </Button>
+
           <ModeToggle />
         </div>
       </div>
