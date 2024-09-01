a0ea6fc1a8769cb6122e49a2bdbe10885f0b6077
Sabine Sun <sabine.sun@hotmail.com>
2024-09-01 15:41:38 +0200

    fix: project link

--- components/section/project-section.tsx
+++ components/section/project-section.tsx
@@ -7,35 +7,41 @@ export const ProjectSection = () => {
   const { projects } = resume;
 
   return projects.map((project) => {
+    const ProjectContent = (
+      <div className="flex flex-1 items-center gap-4">
+        <Image
+          alt={`${project.name} Logo`}
+          className="h-8 w-8"
+          height={32}
+          src={project.logo}
+          width={32}
+        />
+        <div className="flex flex-1 flex-col items-start">
+          <div className="flex w-full flex-row justify-between">
+            <h3 className="leading-6 group-hover:underline">{project.name}</h3>
+            <p className="text-xs text-muted-foreground">{project.year}</p>
+          </div>
+          <p className="text-xs font-light text-muted-foreground">
+            {project.description}
+          </p>
+        </div>
+      </div>
+    );
+
     return (
       <Button
         asChild
-        className=" flex h-fit items-center p-4 "
+        className={`flex h-fit items-center p-4 ${!project.url && "hover:bg-white"}`}
         key={project.name}
         variant="ghost"
       >
-        <Link href={project.url}>
-          <div className="flex flex-1 items-center gap-4">
-            <Image
-              alt="Canal Plus Logo"
-              className="h-8 w-8"
-              height={32}
-              src={project.logo}
-              width={32}
-            />
-            <div className="flex flex-1 flex-col items-start">
-              <div className="flex w-full flex-row justify-between ">
-                <h3 className="leading-6 group-hover:underline">
-                  {project.name}
-                </h3>
-                <p className="text-xs text-muted-foreground">{project.year}</p>
-              </div>
-              <p className="text-xs font-light text-muted-foreground">
-                {project.description}
-              </p>
-            </div>
-          </div>
-        </Link>
+        {project.url ? (
+          <Link href={project.url} rel="noopener noreferrer" target="_blank">
+            {ProjectContent}
+          </Link>
+        ) : (
+          ProjectContent
+        )}
       </Button>
     );
   });
