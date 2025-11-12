1f9f3f5fca9f3abac760a1e29ee957ceb50c3cf4
Sabine Sun <sabine.sun@hotmail.com>
2025-11-12 17:11:49 +0100

    chore: configure eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import eslint from "@eslint/js";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+
+  // Put Prettier to turn off conflicting stylistic rules
+  prettier,
+]);
+
+export default eslintConfig;
