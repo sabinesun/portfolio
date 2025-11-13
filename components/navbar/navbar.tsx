6bb244227d933a00ee001831b18d67cb6a1cb573
Sabine Sun <sabine.sun@hotmail.com>
2025-11-13 22:57:27 +0100

    feat: add new navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,52 +1,17 @@
 import { ModeToggle } from "@/components/theme/mode-toggle";
-import { Button } from "@/components/ui/button";
-import {
-  EnvelopeClosedIcon,
-  GitHubLogoIcon,
-  LinkedInLogoIcon,
-} from "@radix-ui/react-icons";
-import Image from "next/image";
-import Link from "next/link";
-import resume from "@/resume.json";
 import { getTranslations } from "next-intl/server";
 import { LanguageToggle } from "./language-toggle";
-import { Separator } from "../ui/separator";
 
 const Navbar = async () => {
-  const t = await getTranslations("resume");
-  const { basics } = resume;
+  const t = await getTranslations("common");
 
   return (
     <nav className="m-10 flex justify-center">
       <div className="flex w-full items-center justify-between md:max-w-xl">
-        <div className="flex">
-          <div className="relative mr-2 size-14">
-            <Image alt="" fill src="/logo/me.svg" />
-          </div>
-          <div className="flex flex-col">
-            <h1 className="text-lg font-bold">{t(basics.nameKey)}</h1>
-            <p className="whitespace-nowrap text-xs font-light leading-3 text-muted-foreground">
-              {t(basics.labelKey)}
-            </p>
-          </div>
+        <div className="flex gap-4">
+          <p className="capitalize">{t("portfolio")}</p>
         </div>
         <div className="flex items-center gap-0.5">
-          <Button asChild className="w-9 p-0" variant="ghost">
-            <Link href={`mailto: ${basics.email}`}>
-              <EnvelopeClosedIcon className="h-[1.2rem] w-[1.2rem]" />
-            </Link>
-          </Button>
-          <Button asChild className="w-9 p-0" variant="ghost">
-            <Link href={basics.socialMedia.linkedIn.url}>
-              <LinkedInLogoIcon className="h-[1.2rem] w-[1.2rem]" />
-            </Link>
-          </Button>
-          <Button asChild className="w-9 p-0" variant="ghost">
-            <a href={basics.socialMedia.github.url}>
-              <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
-            </a>
-          </Button>
-          <Separator orientation="vertical" className="h-6" />
           <LanguageToggle />
           <ModeToggle />
         </div>
