5d5409ce5374cf9b09a5da50ca2118ce706c98dd
Sabine Sun <sabine.sun@hotmail.com>
2025-12-11 23:13:15 +0100

    feat: add a leaf to Kiosk on hover

--- features/resume/resume-description.tsx
+++ features/resume/resume-description.tsx
@@ -8,21 +8,47 @@ import {
 } from "@radix-ui/react-icons";
 import { getTranslations } from "next-intl/server";
 import Link from "next/link";
+import { type ReactNode } from "react";
 
 export const ResumeDescription = async () => {
   const t = await getTranslations("resume-description");
+  const company = (chunks: ReactNode) => (
+    <span className="relative inline-flex items-center gap-1">
+      <span className="inline-block translate-x-4 opacity-0 transition duration-200 ease-out group-hover:translate-x-0 group-hover:opacity-100 ">
+        🍃
+      </span>
+      <span className="inline-block -translate-x-4  transition  duration-200 ease-out group-hover:translate-x-0 group-hover:font-medium group-hover:text-green-700">
+        {chunks}
+      </span>
+    </span>
+  );
 
   return (
     <div className="text-sm">
       <div className="flex flex-col gap-1">
-        <div className="flex h-6 items-center gap-4">
+        <Link
+          className="group flex h-6 items-center gap-4 text-foreground"
+          href="https://www.meetkiosk.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
           <CodeIcon className="ml-2 size-5 shrink-0" />
-          <p className="text-nowrap">{t("headline")}</p>
-        </div>
-        <div className="flex h-6 items-center gap-4">
-          <HomeIcon className="ml-2 size-5 shrink-0" />
-          <p>{t("location")}</p>
-        </div>
+          <span>{t.rich("headline", { company })}</span>
+        </Link>
+        <Button
+          asChild
+          variant="link"
+          className="flex h-6 w-fit items-center gap-4 p-0 font-light"
+        >
+          <Link
+            href="https://www.google.com/maps/place/Paris/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <HomeIcon className="ml-2 size-5 shrink-0" />
+            <p>{t("location")}</p>
+          </Link>
+        </Button>
         <Button
           asChild
           variant="link"
@@ -30,7 +56,7 @@ export const ResumeDescription = async () => {
         >
           <Link href="mailto:sabine.sun@protonmail.com">
             <EnvelopeClosedIcon className="ml-2 size-5 shrink-0" />
-            <p>sabine.sun@protonmail.com</p>
+            <p>{t("email")}</p>
           </Link>
         </Button>
         <Button
@@ -38,9 +64,13 @@ export const ResumeDescription = async () => {
           variant="link"
           className="flex h-6 w-fit items-center gap-4 p-0 font-light"
         >
-          <Link href="https://www.linkedin.com/in/sabinesun/" target="_blank">
+          <Link
+            href="https://www.linkedin.com/in/sabinesun/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <LinkedInLogoIcon className="ml-2 size-5 shrink-0" />
-            <p>linkedin.com/in/sabinesun</p>
+            <p>{t("linkedin")}</p>
           </Link>
         </Button>
         <Button
@@ -48,9 +78,13 @@ export const ResumeDescription = async () => {
           variant="link"
           className="flex h-6 w-fit items-center gap-4 p-0 font-light"
         >
-          <Link href="https://github.com/sabinesun" target="_blank">
+          <Link
+            href="https://github.com/sabinesun"
+            target="_blank"
+            rel="noreferrer"
+          >
             <GitHubLogoIcon className="ml-2 size-5 shrink-0" />
-            <p>github.com/sabinesun</p>
+            <p>{t("github")}</p>
           </Link>
         </Button>
       </div>
