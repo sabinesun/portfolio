5aadc08fe3334236fe54773607fe7731ec707187
Sabine Sun <sabine.sun@hotmail.com>
2025-12-14 17:28:28 +0100

    fix: heart icon size

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -9,7 +9,7 @@ const Footer = async () => {
       <div className="flex items-center gap-1 md:max-w-xl">
         <p className="text-xs">{t("made-with")}</p>
         <span className="inline-flex h-5 w-5 items-center justify-center">
-          <HeartIcon className="size-5 shrink-0 scale-90 fill-transparent transition duration-200 ease-out hover:scale-100 hover:fill-red-500 hover:text-red-500" />
+          <HeartIcon className="size-5 shrink-0 scale-80 fill-transparent transition duration-200 ease-out hover:scale-100 hover:fill-red-500 hover:text-red-500" />
         </span>
       </div>
     </footer>
